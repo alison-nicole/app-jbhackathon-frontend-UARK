39aac735429a6c7610d5c66d75e4441d9b074bb7
fxnggg <74802761+fxnggg@users.noreply.github.com>
2023-04-07 15:53:47 -0500

    Styling changes to admin login

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,13 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  adminLoginForm: FormGroup;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, readonly fb: FormBuilder) { }
 
   ngOnInit() {
+    this.adminLoginForm = this.fb.group({
+      'email': new FormControl('', Validators.compose([Validators.required, Validators.email])),
+      'password': new FormControl('', Validators.compose([Validators.required, Validators.maxLength(30)])),
+    })
   }
 
   routeTo(target: string) {
     this.router.navigateByUrl(target);
+  }
+
+  next(start: string, end: string) {
+    document.getElementById(start).classList.toggle('hidden');
+    document.getElementById(end).classList.toggle('hidden');
+  }
+
+  back(start: string, end: string) {
+    document.getElementById(start).classList.toggle('hidden');
+    document.getElementById(end).classList.toggle('hidden');
   }
 }
