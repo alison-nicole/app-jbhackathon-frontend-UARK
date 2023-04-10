37886a44a5b19df2358693863287bbf08cdd2ec3
fxnggg <74802761+fxnggg@users.noreply.github.com>
2023-04-10 17:28:14 -0500

    Created prizes page

--- src/app/features/hackathon-sign-up/prizes/prizes.component.ts
+++ src/app/features/hackathon-sign-up/prizes/prizes.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-prizes',
   templateUrl: './prizes.component.html',
-  styleUrls: ['./prizes.component.css']
+  styleUrls: ['./prizes.component.scss']
 })
 export class PrizesComponent implements OnInit {
+
+  noBuilderPageForUrl: boolean = false;
 
   constructor() { }
 
