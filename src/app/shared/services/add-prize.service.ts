c43a622b49b57fc7eb1d425e2cd4701d047bd7ed
fxnggg <74802761+fxnggg@users.noreply.github.com>
2023-04-11 21:42:51 -0500

    Create add prize service files

---
+++ src/app/shared/services/add-prize.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Cache-Control': 'no-cache,no-store,max-age=0,must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': '-1',
+    'Strict-Transport-Security': 'max-age=15552000; includeSubDomains; preload',
+    'X-XSS-Protection': '1;mode=block',
+    'X-Content-Type-Options': 'nosniff'
+  })
+};
+@Injectable({
+  providedIn: 'root'
+})
+export class AddPrizeService {
+
+  constructor(readonly http: HttpClient) { }
+
+  readonly getAll = 'all';
+  readonly apiRoot = '/api/ws_infrastructure_university_hackathon';
+
+}
